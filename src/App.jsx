607e079e2b1a5daa818fd7d8aff2f3c67e4186e8
Victoria Durand <you@example.comvictoria.durand.art@gmail.com>
2025-02-26 19:36:31 -0500

    Change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./index.css";
 import { useState } from "react";
-import { LoadingScreen } from "./components/LoadingScreen";
+{
+  /*import { LoadingScreen } from "./components/LoadingScreen";*/
+}
 import { Navbar } from "./components/Navbar";
 import { MobileMenu } from "./components/MobileMenu";
 import { Home } from "./components/sections/Home";
@@ -16,12 +18,14 @@ import { Clients } from "./components/sections/Clients";
 import { Footer } from "./components/Footer";
 
 function App() {
-  const [isLoaded, setIsLoaded] = useState(false);
+  {
+    /*const [isLoaded, setIsLoaded] = useState(false);*/
+  }
   const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div>
-      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
-      {""}
+      {/*{!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      {""}*/}
       <div
         className={`min-h-screen transition-opacity duration-700 ${
           isLoaded ? "opacity-100" : "opacity-0"
